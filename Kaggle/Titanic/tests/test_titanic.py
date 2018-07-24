b1d9a19174f3a85268f7b11bc7afa14d60b75d64
John Kwening <kwening.john@gmail.com>
2018-07-23 21:36:49 -0400

    chore: Add initial titanic folder struture and files

--- Kaggle/Titanic/tests/test_titanic.py
+++ Kaggle/Titanic/tests/test_titanic.py
@@ -3,8 +3,12 @@ from .. import titanic
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_(self):
+
+
+    def test_load_training_set(self):
+        result = titanic.load_training_set()
+        self.assertTrue(result, 'Null pandas data object returned')
 
 
 if __name__ == '__main__':
--- Kaggle/Titanic/titanic.py
+++ Kaggle/Titanic/titanic.py
@@ -2,4 +2,32 @@
 
 import os
 import zipfile
+import pandas as pd
 
+# Module constants
+DATASET_DIR = os.path.join(os.path.abspath(__file__), 'datasets')
+TRAINING_CSV =  os.path.join(DATASET_DIR, 'train.csv')
+TEST_CSV = os.path.join(DATASET_DIR, 'test.csv')
+DATA_ZIP = os.path.join(DATASET_DIR, 'all.zip')
+
+
+# get dataset if flat file not readily available
+def get_csv_from_zip(train_set=True):
+    csv = TRAINING_CSV if train_set else TEST_CSV
+
+    with zipfile.ZipFile(DATA_ZIP) as data_zip:
+        member = 'train.csv' if train_set else 'test.csv'
+        data_zip.extract(member, path=DATASET_DIR)
+
+
+# load data via pandas
+def load_dataset(csv=TRAINING_CSV):
+    if not os.path.isfile(csv):
+        get_csv_from_zip()
+
+    return pd.read_csv(csv)
+
+############################################
+# Explore the data
+############################################
+train_set = load_dataset()
