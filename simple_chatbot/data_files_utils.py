ce0cd4ce577ab9b90c45c7ba99e75e7346f55f72
John Kwening <kwening.john@gmail.com>
2019-05-12 10:48:26 -0400

    feat: voc.py
    Add Voc class for housing vocabulary words for given corpus.
    Add utility functions for generating vocabulary words and movie line pairs.

--- simple_chatbot/data_files_utils.py
+++ simple_chatbot/data_files_utils.py
@@ -1,17 +1,27 @@
 import os
 import codecs
 import csv
+import unicodedata
+import re
+import pickle
+from datetime import datetime
 
+# Project modules
+from simple_chatbot.voc import Voc
+
+TIMESTAMP = datetime.now().strftime('%m%d%Y')
 ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
+SAVE_DIR = os.path.join(ROOT_DIR, 'data', 'save')
 CORPUS_DIR = os.path.join(ROOT_DIR, 'data', 'cornell_movie_dialogs_corpus')
 MOVIE_LINES_PATH = os.path.join(CORPUS_DIR, 'movie_lines.txt')
 MOVIE_CONVERSATIONS_PATH = os.path.join(CORPUS_DIR, 'movie_conversations.txt')
 FORMATTED_MOVIE_LINES_PATH = os.path.join(CORPUS_DIR, 'formatted_movie_lines.txt')
 MOVIE_LINES_FIELDS = ['lineID', 'characterID', 'movieID', 'character', 'text']
 MOVIE_CONVOS_FIELDS = ['character1ID', 'character2ID', 'movieID', 'utteranceIDs']
+SENTENCE_MAX_LENGTH = 10
 
 
-def print_lines(file, num_lines=10) -> None:
+def print_lines(file: str, num_lines=10) -> None:
     with open(file, 'rb') as f:
         lines = f.readlines()
 
@@ -19,7 +29,7 @@ def print_lines(file, num_lines=10) -> None:
         print(line)
 
 
-def load_lines(file, fields: list) -> dict:
+def load_lines(file: str, fields: list) -> dict:
     """Parse each line of file into dictionary of fields."""
     lines = dict()
     with open(file, 'r', encoding='iso-8859-1') as f:
@@ -33,7 +43,7 @@ def load_lines(file, fields: list) -> dict:
     return lines
 
 
-def load_convos(file, lines, fields) -> list:
+def load_convos(file: str, lines: list, fields: list) -> list:
     """
     Groups fields of lines from load_lines() into conversations based o
      move_conversations.txt
@@ -58,7 +68,7 @@ def load_convos(file, lines, fields) -> list:
     return convos
 
 
-def extract_sentence_pairs(conversations):
+def extract_sentence_pairs(conversations: list) -> list:
     """Extracts pairs of sentences from conversations."""
     pairs = list()
     for conv in conversations:
@@ -93,3 +103,104 @@ def format_movie_lines(save_file_as='formatted_movie_lines.txt'):
     # Print a sample of lines
     print('\nSample lines from file:')
     print_lines(save_file_as)
+
+
+def unicode_to_ascii(s: str) -> str:
+    """Turn a Unicode string to plain ASCII.
+
+    Thanks to: https://stackoverflow.com/a/518232/2809427
+    """
+    return ''.join(
+        [c for c in unicodedata.normalize('NFD', s)
+         if unicodedata.category(c) != 'Mn']
+    )
+
+
+def normalize_str(s: str) -> str:
+    """
+    Normalize string by converting to lowercase, trimming, and removing
+    non-letter characters.
+    """
+    s = unicode_to_ascii(s.lower().strip())
+    s = re.sub(r'([.!?])', r' \1', s)
+    s = re.sub(r'[^a-zA-Z.!?]+', r' ', s)
+    s = re.sub('\s+', r' ', s).strip()
+    return s
+
+
+def read_vocs(formatted_data_file_path, corpus_name):
+    """Read query/response pairs.
+
+    Returns:
+        voc, pairs: tuple Voc object and pairs of normalized lines.
+    """
+    print('\nReading lines...')
+
+    # Read the file and split into lines
+    lines = open(formatted_data_file_path, encoding='utf-8').read().strip().split('\n')
+
+    # Split every line into pairs and normalize
+    pairs = [[normalize_str(s) for s in l.split('\t')] for l in lines]
+    voc = Voc(corpus_name)
+    return voc, pairs
+
+
+def filter_pair(p: str) -> bool:
+    """
+    Returns True iff both sentences in a pair 'p' are under
+    the SENTENCE_MAX_LENGTH threshold.
+    """
+    return len(p[0].split()) < SENTENCE_MAX_LENGTH and \
+           len(p[1].split()) < SENTENCE_MAX_LENGTH
+
+
+def filter_pairs(pairs: list) -> list:
+    """Filter pairs using filter_pair() condition."""
+    return [pair for pair in pairs if filter_pair(pair)]
+
+
+def assemble_voc_pairs(corpus_name: str, formatted_data_file_path: str,
+                       save=True) -> tuple:
+    """
+    Process formatted movie lines data file and save voc obj
+    and pairs by default.
+
+    Args:
+        corpus_name: Name of the corpus.
+        formatted_data_file_path: The path for the formatted movie lines txt.
+        save: If True save voc object and pairs list as pickle file.
+
+    Returns:
+        voc, pairs: tuple Voc object and pairs of normalized lines.
+    """
+    print('\nStart preparing training data...')
+    voc, pairs = read_vocs(formatted_data_file_path, corpus_name)
+    print('\tRead {!s} sentence pairs'.format(len(pairs)))
+    pairs = filter_pairs(pairs)
+    print('\tTrimmed to {!s} sentence pairs'.format(len(pairs)))
+    print('\nCounting words...')
+    for pair in pairs:
+        voc.add_sentence(pair[0])
+        voc.add_sentence(pair[1])
+    print('\tCounted words:', voc.get_num_words())
+
+    # Print some pairs to validate
+    print('\nPairs:')
+    for pair in pairs[:10]:
+        print(pair)
+
+    if save:
+        print('\nSaving voc object via pickle...')
+        with open(os.path.join(SAVE_DIR, f'voc_{TIMESTAMP}'), mode='wb') as f:
+            pickle.dump(voc, f)
+
+        print('\nSaving pairs list via pickle...')
+        with open(os.path.join(SAVE_DIR, f'pairs_{TIMESTAMP}'), mode='wb') as f:
+            pickle.dump(pairs, f)
+
+    return voc, pairs
+
+
+voc, pairs = assemble_voc_pairs(corpus_name='Cornell Movie Dialogues',
+                                formatted_data_file_path=FORMATTED_MOVIE_LINES_PATH,
+                                save=True)
---
+++ simple_chatbot/voc.py
@@ -0,0 +1,74 @@
+# Default word tokens
+PAD_TOKEN = 0  # Used for padding short sentences
+SOS_TOKEN = 1  # Start-of-sentence token
+EOS_TOKEN = 2  # End-of-sentence token
+
+
+class Voc(object):
+    """
+    Voc class keeps a mapping from words to indices, a reverse mapping of
+    indices to words, a count of each word, and a total word count.
+    """
+    def __init__(self, name):
+        self._name = name
+        self._trimmed = False
+        self._word2index = dict()
+        self._word2count = dict()
+        self._index2word = {
+            PAD_TOKEN: 'PAD',
+            SOS_TOKEN: 'SOS',
+            EOS_TOKEN: 'EOS'
+        }
+        self._num_words = 3  # Count SOS, EOS, PAD
+
+    def get_num_words(self) -> int:
+        return self._num_words
+
+    def add_word(self, word):
+        """Add a word to the vocabulary."""
+        if word not in self._word2index:
+            self._word2index[word] = self._num_words
+            self._word2count[word] = 1
+            self._index2word[self._num_words] = word
+            self._num_words += 1
+        else:
+            self._word2count[word] += 1
+
+    def add_sentence(self, sentence):
+        """Add all words in a sentence to vocabulary."""
+        for word in sentence.split():
+            self.add_word(word)
+
+    def trim(self, min_count):
+        """
+        Trim infrequently seen words by removing words below a
+        certain count threshold.
+        """
+        if self._trimmed:  # Skip if vocabulary already trimmed
+            return
+
+        self._trimmed = True
+        keep_words = list()
+
+        for k, v in self._word2count.items():
+            if v >= min_count:
+                keep_words.append(k)
+
+        print('keep words {} / {} = {:.4f}'.format(
+            len(keep_words), len(self._word2index),
+            len(keep_words) / len(self._word2index)
+        ))
+
+        # Re-initialize dictionaries
+        self._word2index = dict()
+        self._word2count = dict()
+        self._index2word = {
+            PAD_TOKEN: 'PAD',
+            SOS_TOKEN: 'SOS',
+            EOS_TOKEN: 'EOS'
+        }
+        self._num_words = 3  # Count default tokens
+
+        # Update vocabulary with kept words
+        for word in keep_words:
+            self.add_word(word)
